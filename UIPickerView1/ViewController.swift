6d7bf1ad330426d6f6a853d4327e0b50c522a10e
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-04-19 14:59:49 +0900

    add picker

--- UIPickerView1/ViewController.swift
+++ UIPickerView1/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet weak var myPickerView: UIPickerView!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var lbl: UILabel!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 2
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        if component == 0 {
+            return 10
+        }
+        else {
+            return 100
+        }
+        
+    }
+    //UIPickerViewDelegate method
+    // 1000번 호출
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        if component == 0 {
+            return "comp01  \(row)"
+        } else {
+            return "comp02  \(row)"
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        if component == 0 {
+            myLabel.text = "comp01 \(row)"
+        } else {
+            lbl.text = "comp02 \(row)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Delegate, DataSource 연결
+        myPickerView.dataSource = self
+        myPickerView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
